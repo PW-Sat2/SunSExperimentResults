3a563670694db7424cf7f06007292a88aef58708
begreeen <michal.gumiela@gmail.com>
2019-07-20 15:29:38 +0200

    gyro from suns

--- gyroscope/filter_gyroscope/filter_gyroscope.m
+++ gyroscope/filter_gyroscope/filter_gyroscope.m
@@ -1,7 +1,24 @@
 clear all;
 close all;
 
-datanames = {'sunsps5'};
+datanames = {
+%          'suns1',
+%          'suns2',
+%          'suns3_time_corrected',
+%          'suns4',
+%          'suns5',
+%          'suns6',
+%          'suns7',
+%          'suns8',
+%          'sunsps1',
+%          'sunsps2',
+%          'sunsps3',
+%          'sunsps4',
+%          'sunsps5',
+         'sunsps6',
+         'sunsps7',
+         'sunsps8',
+         'sunsps9'};
 
 for k=1:length(datanames)
     dataname = datanames{k}
