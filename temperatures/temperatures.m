f3f8ad7c8c2c94ebc323bd65747267b4c6995760
begreeen <michal.gumiela@gmail.com>
2019-08-12 23:10:48 +0200

    temperatures plots

---
+++ temperatures/temperatures.m
@@ -0,0 +1,44 @@
+clear all;
+close all;
+
+datanames = {
+         'suns1'
+         'suns2'
+         'suns3_time_corrected'
+         'suns4'
+         'suns5'
+         'suns6'
+         'suns7'
+         'suns8'
+         'sunsps1'
+         'sunsps2'
+         'sunsps3'
+         'sunsps4'
+         'sunsps5'
+         'sunsps6'
+         'sunsps7'
+         'sunsps8'
+         'sunsps9'
+         'sunsps10'};
+
+for k=1:length(datanames)
+    dataname = datanames{k}
+    load(strcat('..\experiment_data\matlab\', dataname, '.mat'));
+
+    temp = figure('Renderer', 'painters', 'Position', [10 10 1200 900]);
+    title(strcat('Temperature ALS panels: ', dataname));
+    ylabel('Temperature [°C]');
+    xlabel('Time [min]');
+    hold on;
+    plot(suns.timestamp/60, suns.SunStemperature_a, '.-');
+    plot(suns.timestamp/60, suns.SunStemperature_b, '.-');
+    plot(suns.timestamp/60, suns.SunStemperature_c, '.-');
+    plot(suns.timestamp/60, suns.SunStemperature_d, '.-');
+    plot(suns.timestamp/60, suns.SunStemperature_structure, '.-');
+    legend('A', 'B', 'C', 'D', 'Structure', 'Location', 'best');
+    grid on;
+
+    print(temp,strcat('outputs/', '/temperatures-', dataname, '.png'),'-dpng','-r600');
+    close all;
+
+end
